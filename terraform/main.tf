59a5ae0c6075c2528e9fc1da0d87f6afb76a8e05
venkata <venkata@gmail.com>
2026-03-16 21:52:38 +0530

    modified

--- terraform/main.tf
+++ terraform/main.tf
@@ -26,8 +26,6 @@ resource "azurerm_app_service_plan" "appserviceplan" {
   location = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
 kind = "Windows"
-reserved = true
-
 sku {
   tier = "Basic"
   size = "B1"
